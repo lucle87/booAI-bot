b6970c3ac1cbcacddb3a29f6f15345054695104e
Lực Lê <vanlucpdu@gmail.com>
2026-06-06 09:39:16 +0700

    fix privy missing deps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@stripe/crypto': false,
+      '@farcaster/mini-app-solana': false,
+    }
+    return config
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
